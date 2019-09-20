fbcba8a37bdc92635e95f0c23c14748dcd734309
ladaegorova18 <st068843@student.spbu.ru>
2019-09-20 20:50:17 +0300

    forks and philosophers

---
+++ Philosophers/Philosophers/Philosopher.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace Philosophers
+{
+    public class Philosopher
+    {
+        private object fork1;
+        private object fork2;
+        private Random rnd = new Random();
+        private int number;
+
+        private const int minTime = 0;
+        private const int maxTime = 1;
+
+        private int countPhilosophers;
+        public bool Alive { get; set; } = true;
+
+        public Philosopher(object[] forks, int i)
+        {
+            number = i;
+            fork1 = forks[i];
+            countPhilosophers = forks.Length;
+            if (i + 1 >= forks.Length)
+            {
+                fork1 = forks[0];
+                fork2 = forks[i];
+                return;
+            }
+            fork2 = forks[i + 1];
+        }
+
+        public void Live()
+        {
+            while (Alive)
+            {
+                Console.WriteLine("eating");
+                Eat();
+                Think();
+            }
+        }
+
+        private void Eat()
+        {
+            Console.WriteLine($"I am a philosopher number {number} and take a fork number {number}");
+            lock (fork1)
+            {
+                Console.WriteLine($"I am a philosopher number {number} and take a fork number {(number + 1) % countPhilosophers}");
+                lock (fork2)
+                {
+                    Thread.Sleep(rnd.Next(minTime, maxTime));
+                }
+            }
+        }
+
+        private void Think()
+        {
+            Console.WriteLine("I am a philosopher number: " + number);
+            Thread.Sleep(rnd.Next(minTime, maxTime));
+        }
+    }
+}
---
+++ Philosophers/Philosophers/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace Philosophers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var n = 10;
+            var forks = new object[n];
+            var philosophers = new Philosopher[n];
+            var threads = new Thread[n];
+            for (var i = 0; i < n; ++i)
+            {
+                forks[i] = new object();
+            }
+            for (var i = 0; i < n; ++i)
+            {
+                philosophers[i] = new Philosopher(forks, i);
+                threads[i] = new Thread(philosophers[i].Live);
+                threads[i].Start();
+            }
+            Console.ReadLine();
+            foreach (var cell in philosophers)
+            {
+                cell.Alive = false;
+            }
+        }
+    }
+}
